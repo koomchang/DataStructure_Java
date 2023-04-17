74b57b56565834a4e380bda3bc83c70c0dee012d
koomchang <leahpar0401@gmail.com>
2023-04-17 21:27:23 +0900

    add: LinkedList Constructor

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,45 @@
+package LinkedList;
+
+public class LinkedList {
+    int length;
+    Node head;
+    Node tail;
+
+    public LinkedList(int value){
+        Node newNode = new Node(value);
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1;
+    }
+
+    static class Node{
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void getLength() {
+        System.out.println("Length : " + length);
+    }
+
+    public void getHead() {
+        System.out.println("Head : " + head.value);
+
+    }
+
+    public void getTail() {
+        System.out.println("Tail : " + tail.value);
+    }
+
+    public void printLinkedList(){
+        Node temp = head;
+        while (temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+}
+
---
+++ src/LinkedList/Main.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(4);
+        linkedList.getHead();
+        linkedList.getTail();
+        linkedList.getLength();
+
+        linkedList.printLinkedList();
+    }
+
+}
